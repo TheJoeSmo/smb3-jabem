91cb6c43d2ffa532e1dc21480d577a9100b05390
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-08-19 00:34:44 -0400

    Remove named labels
    Helps with compatibilty

--- src/custom/para_boss/action_fly_above_player.asm
+++ src/custom/para_boss/action_fly_above_player.asm
@@ -8,15 +8,15 @@ para_boss_action_fly_above_player:
     JSR Level_ObjCalcYDiffs
 
     CPY #$00
-    BNE PRG004_B011  ; If Player is higher than Paragoomba, jump to PRG004_B011
+    BNE +  ; If Player is higher than Paragoomba, jump to +
 
     LDA Temp_Var16
     CMP #$38
-    BLT PRG004_B011  ; If Paragoomba is not sufficiently above Player, jump to PRG004_B011
+    BLT +  ; If Paragoomba is not sufficiently above Player, jump to +
 
     INC Objects_YVel,X  ; A little lower
 
-    BNE PRG004_B015     ; If Paragoomba hit zero, jump to PRG004_B015
+    BNE ++     ; If Paragoomba hit zero, jump to ++
 
     INC Objects_Var4,X ; Var4++ (next internal state)
 
@@ -26,38 +26,37 @@ para_boss_action_fly_above_player:
 
     RTS      ; Return
 
-PRG004_B011:
++:
 
     ; Fly higher
     LDA #-$10
     STA Objects_YVel,X
 
-PRG004_B015:
+++:
     JSR Object_ApplyYVel_NoLimit     ; Apply Y velocity
 
-PRG004_B046:
     LDY #$00     ; Y = 0
 
     LDA Objects_FlipBits,X
-    BNE PRG004_B04E  ; If flipped, jump to PRG004_B04E
+    BNE +  ; If flipped, jump to +
 
     INY      ; Y = 1
 
-PRG004_B04E:
++:
     LDA Objects_XVel,X
     CMP Paragoomba_XVelLimit,Y
-    BEQ PRG004_B05B     ; If Paragoomba is at his X velocity limit, jump to PRG004_B05B
+    BEQ +     ; If Paragoomba is at his X velocity limit, jump to +
 
     CLC
     ADC Paragoomba_XVelAccel,Y
     STA Objects_XVel,X  ; Accelerate Paragoomba
 
-PRG004_B05B:
++:
     INC Objects_Var2,X   ; Var2++
 
     LDA Objects_Var2,X
     AND #$3f
-    BNE PRG004_B06E  ; 1:64 ticks proceed, otherwise jump to PRG004_B06E
+    BNE +  ; 1:64 ticks proceed, otherwise jump to +
 
     JSR Level_ObjCalcXDiffs
 
@@ -65,6 +64,6 @@ PRG004_B05B:
     LDA GroundTroop_FlipTowardsPlayer,Y
     STA Objects_FlipBits,X
 
-PRG004_B06E:
++:
     JSR Object_ApplyXVel     ; Apply X Velocity
     JMP Object_ApplyYVel_NoLimit     ; Apply Y velocity and don't come back!
