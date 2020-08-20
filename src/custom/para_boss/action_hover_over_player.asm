d024a8b9a86667168d90422c9b0976d5d164d8ca
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-08-20 17:44:28 -0400

    Refactor handle_vertical_velocity to allow for different occilations

--- src/custom/para_boss/action_hover_over_player.asm
+++ src/custom/para_boss/action_hover_over_player.asm
@@ -2,6 +2,8 @@
 
 
 para_boss_hover_over_player:
+    LDA #$00
+    STA Temp_Var14      ; Slowly go above player and regular vertical speeds
     JSR para_boss_handle_vertical_velocity
     JSR para_boss_handle_horizontal_velocity
     JMP para_boss_apply_velocity
--- src/custom/para_boss/handle_vertical_velocity.asm
+++ src/custom/para_boss/handle_vertical_velocity.asm
@@ -1,4 +1,10 @@
 
+para_boss_vertical_distance_ranges:
+    .byte $18, $1A, $1E, $00
+
+para_boss_vertical_distance_speeds:
+    .byte $01, $03, $04, $07
+
 
 para_boss_rise_quickest:
     DEC objects_y_velocity, x  
@@ -41,41 +47,51 @@ para_boss_lower:
     RTS
 
 
+; Temp_Var14 determines the offset and the high bit determines how fast to fly above the player
 para_boss_handle_vertical_velocity:
-    LDY #$00
+    LDA Temp_Var14
+    AND #%01111111
+    TAY
     JSR Object_CalcCoarseYDiff
 
+    LDA Temp_Var14
+    AND #%10000000
+    BEQ +
     LDA Temp_Var15
-    BPL para_boss_rise_quickest     ; If below player, get above them
-
+    BPL para_boss_rise_quickest     ; If below player, get above them no matter what
+    JMP ++
++
     LDA Temp_Var15
-    CLC
-    ADC #$18
-    BCS +
-
-    CLC
-    ADC #$02
-    BCS ++
-
-    CLC
-    ADC #$04
-    BCS +++
-
-    INY
-
-+++
-    INY
+    BPL para_boss_rise              ; Slower rise above the player
 
 ++
+    LDA Temp_Var15
+    CLC
+    ADC para_boss_vertical_distance_ranges, y
+    BCS +
+
+-
     INY
 
+    LDA para_boss_vertical_distance_ranges, y
+    BEQ +
+
+    LDA Temp_Var15
+    CLC
+    ADC para_boss_vertical_distance_ranges, y
+    BCS +
+    BCC -
+
 +
-
-    TYA
-
+    LDA para_boss_vertical_distance_speeds, y
     JSR DynJump
 
+    .word para_boss_do_nothing
     .word para_boss_rise_quickest
+    .word para_boss_rise_quicker
     .word para_boss_rise
+
+    .word para_boss_do_nothing
     .word para_boss_lower
+    .word para_boss_lower_quicker
     .word para_boss_lower_quickest
