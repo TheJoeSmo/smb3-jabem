e7b5e60f281ad840cbccaca5265e19dfc6c39da6
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-08-20 19:25:00 -0400

    Add changing state

--- src/custom/para_boss/action_hover_over_player.asm
+++ src/custom/para_boss/action_hover_over_player.asm
@@ -2,6 +2,35 @@
 
 
 para_boss_hover_over_player:
+
+; Determine if we need to change state
+    LDA objects_timer, x
+    BNE +
+
+; Change state (updates next frame)
+    JSR Object_CalcCoarseXDiff
+
+    LDA Temp_Var15
+    CLC
+    ADC #$20
+    CMP #$40
+    BLT +
+
+; Insure the player is fairly close to the boss
+    LDA #$06
+    STA para_state, x    
+
++
+
+; Calculate the frame we are on (between 0 and 1)
+    LDA tick_counter
+    AND #%00000100
+    CLC
+    ROR A
+    ROR A 
+    STA objects_animation_frame, x
+
+; Calculate velocities
     LDA #$00
     STA Temp_Var14      ; Slowly go above player and regular vertical speeds
     JSR para_boss_handle_vertical_velocity
