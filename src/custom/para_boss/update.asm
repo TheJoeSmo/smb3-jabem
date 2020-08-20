44e138ce280d9c0a1c0320d89e33291368123e34
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-08-20 19:52:42 -0400

    Add additional actions

--- src/custom/para_boss/update.asm
+++ src/custom/para_boss/update.asm
@@ -13,6 +13,7 @@ para_boss_update:
 
     JSR para_hit_test
 
+para_boss_dirty_update:
     LDA para_state, x
 
     JSR DynJump
@@ -22,8 +23,11 @@ para_boss_update:
     .word para_boss_action_fly_above_player         ; 1: Fly above Player
     .word para_boss_action_summon_micro_goomba      ; 2: Drop Micro goombas on Player
     .word para_boss_action_do_nothing
-    .word para_boss_hover_over_player
 
+    .word para_boss_action_prep_strong_attack       ; 3: Set the time for the next action
+    .word para_boss_hover_over_player               ; 4: Fly around for a while
+    .word para_boss_action_prep_attack              ; 5: Set the time for the next action
+    .word para_boss_action_be_slow                  ; 6: Fly around slowly
 
 
 
