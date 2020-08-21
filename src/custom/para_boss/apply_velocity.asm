7086861dedd80d3dd389146017ffd6088ba8cd02
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-08-20 23:22:14 -0400

    Add ability to try and adjust to enviroment

--- src/custom/para_boss/apply_velocity.asm
+++ src/custom/para_boss/apply_velocity.asm
@@ -1,3 +1,30 @@
 para_boss_apply_velocity:
-    JSR Object_ApplyXVel                ; Apply X Velocity
-    JMP Object_ApplyYVel_NoLimit        ; Apply Y velocity and don't come back!
+    JSR Object_Move
+
+    LDA objects_detection_flags, x
+    AND #object_hit_ceiling
+    BEQ +
+
+; Bounce off ceiling
+    LDA #$01
+    STA objects_y_velocity, x
+
++
+    LDA objects_detection_flags, x
+    AND #object_hit_ground
+    BEQ +
+
+    JSR Object_HitGround     ; Align to floor
+
++
+    LDA objects_detection_flags, x
+    AND #object_hit_wall
+    BEQ +  ; If Paragoomba did not hit wall, jump to +
+
+    JSR Object_FlipFace  ; Turn around
+    LDA #-$02
+    STA objects_x_velocity, x
+
+
++
+    RTS      ; Return
