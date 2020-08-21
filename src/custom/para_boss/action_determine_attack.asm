22b69acf1c0638da7cfde7f207848ef9592bfbf7
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-08-21 17:33:32 -0400

    Create action_determine_attack.asm

---
+++ src/custom/para_boss/action_determine_attack.asm
@@ -0,0 +1,45 @@
+
+
+para_boss_action_determine_attack:
+	LDA cur_random+1
+	AND #%00000111
+
+	JSR DynJump
+
+	.word para_boss_do_spawn_micro_goombas
+	.word para_boss_do_spawn_micro_goombas
+	.word para_boss_do_spawn_micro_goombas
+	.word para_boss_do_spawn_micro_goombas
+	.word para_boss_do_throw_goomba_at_player
+	.word para_boss_do_throw_paragoomba_at_player
+	.word para_boss_do_flying
+	.word para_boss_do_flying
+
+; Shoot between 1 and 4 micro goombas
+para_boss_do_spawn_micro_goombas:
+	LDA cur_random+2
+	AND #%00000011
+	CLC
+	ASL A
+	CLC
+	ADC #$08
+	STA para_state, x
+	JMP para_boss_dirty_update
+
+
+para_boss_do_throw_goomba_at_player:
+	LDA #$13
+	STA para_state, x
+	JMP para_boss_dirty_update
+
+
+para_boss_do_throw_paragoomba_at_player:
+	LDA #$16
+	STA para_state, x
+	JMP para_boss_dirty_update
+
+
+para_boss_do_flying:
+	LDA #$01
+	STA para_state, x
+	JMP para_boss_dirty_update
