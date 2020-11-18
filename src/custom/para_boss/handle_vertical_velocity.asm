04eaa4bde3b273d888d5bd89eb4f5ea0fcedd8cd
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-11-18 01:21:56 -0500

    Add custom flights

--- src/custom/para_boss/handle_vertical_velocity.asm
+++ src/custom/para_boss/handle_vertical_velocity.asm
@@ -1,53 +1,125 @@
 
-para_boss_vertical_distance_ranges:
-    .byte $18, $1A, $1E, $00
-    .byte $14, $1A, $1E, $00
-    .byte $18, $1A, $1E, $00
+para_boss_vertical_distance_ranges: 
+; Bands
+    .byte 10, 12, 14, 16,  18, 20, 22, 24,  26, 28, 30, 32, 60  ; Chaotic Neutral Band  ; 0
+    .byte 10, 12, 14, 16,  18, 20, 22, 24,  26, 28, 30, 32, 60  ; Fall From Heaven Band ; 13
+    .byte 6, 12, 14, 16,   18, 24, 26, 32, 60                   ; Twin Peaks Band       ; 26
 
+; Fast Speeds
+    .byte 18, 24, 26, 32    ; 12.5 blocks above player          ; 31
+    .byte 14, 20, 22, 28    ; 10.5 blocks above player          ; 35
+    .byte 10, 16, 18, 24    ; 8.5 blocks above player           ; 39
+    .byte 6, 12, 14, 20     ; 6.5 blocks above player           ; 43
+    .byte 2, 8, 10, 12      ; 4.5 blocks above player           ; 47
+    .byte 0, 4, 6, 8        ; 2.5 blocks above player           ; 51
+    .byte 60                ; Max value to stop overflow
+
+; Normal Speeds
+    .byte 18, 24, 26, 32    ; 12.5 blocks above player          ; 56
+    .byte 14, 20, 22, 28    ; 10.5 blocks above player          ; 60
+    .byte 10, 16, 18, 24    ; 8.5 blocks above player           ; 64
+    .byte 6, 12, 14, 20     ; 6.5 blocks above player           ; 68
+    .byte 2, 8, 10, 12      ; 4.5 blocks above player           ; 72
+    .byte 0, 4, 6, 8        ; 2.5 blocks above player           ; 76
+    .byte 60                ; Max value to stop overflow
+
+; do_nothing 0 | rise_quickest 1 | rise_quicker 2 | rise 3 | do_nothing 4 | lower 5 | lower_quicker 6 | lower_quickest 7
 para_boss_vertical_distance_speeds:
-    .byte $01, $03, $04, $07
-    .byte $02, $04, $04, $06
-    .byte $02, $03, $05, $06
+; Bands
+    .byte 1, 2, 6, 7, 2, 3, 5, 6, 1, 2, 6, 7, 7     ; Chaotic Neutral Band
+    .byte 2, 3, 5, 6, 2, 3, 5, 7, 2, 3, 6, 7, 7     ; Fall From Heaven Band
+    .byte 2, 3, 5, 6, 2, 3, 5, 6, 7, 7              ; Twin Peaks Band
+
+; Fast Speed
+    .byte 1, 2, 6, 7        ; 12.5 blocks above player
+    .byte 1, 2, 6, 7        ; 10.5 blocks above player
+    .byte 1, 2, 6, 7        ; 8.5 blocks above player
+    .byte 1, 2, 6, 7        ; 6.5 blocks above player 
+    .byte 1, 2, 6, 7        ; 4.5 blocks above player
+    .byte 1, 2, 6, 7        ; 2.5 blocks above player
+    .byte 7                 ; Go down and go down fast
+
+; Normal Speed
+    .byte 2, 3, 5, 6        ; 12.5 blocks above player
+    .byte 2, 3, 5, 6        ; 10.5 blocks above player
+    .byte 2, 3, 5, 6        ; 8.5 blocks above player
+    .byte 2, 3, 5, 6        ; 6.5 blocks above player 
+    .byte 2, 3, 5, 6        ; 4.5 blocks above player
+    .byte 2, 3, 5, 6        ; 2.5 blocks above player
+    .byte 7                 ; Go down and go down fast
 
 
 para_boss_rise_quickest:
-    DEC objects_y_velocity, x  
-    DEC objects_y_velocity, x
+    LDA objects_y_velocity, x
+    SEC
+    SBC #(OBJECT_FALLRATE + 3)
+    STA objects_y_velocity, x
+    JMP para_boss_rise_cap
 
 para_boss_rise_quicker:
-    DEC objects_y_velocity, x
-    DEC objects_y_velocity, x
+    LDA objects_y_velocity, x
+    SEC
+    SBC #(OBJECT_FALLRATE + 2)
+    STA objects_y_velocity, x
+    JMP para_boss_rise_cap
 
 para_boss_rise:
-    DEC objects_y_velocity, x
-
     LDA objects_y_velocity, x
-    CMP #-50
+    SEC
+    SBC #(OBJECT_FALLRATE + 1)
+    STA objects_y_velocity, x
+
+
+para_boss_rise_cap:
+    LDA objects_y_velocity, x
+    CMP #-40
     BPL +
-    LDA #-50
+    LDA #-40
     STA objects_y_velocity, x
 +
 
     RTS
 
-
 para_boss_lower_quickest:
-    INC objects_y_velocity, x
-    INC objects_y_velocity, x
+    LDA objects_y_velocity, x
+    CLC
+    ADC #(3 - OBJECT_FALLRATE)
+    STA objects_y_velocity, x
+    JMP para_boss_lower_cap
 
 para_boss_lower_quicker:
-    INC objects_y_velocity, x
+    LDA objects_y_velocity, x
+    CLC
+    ADC #(2 - OBJECT_FALLRATE)
+    STA objects_y_velocity, x
+    JMP para_boss_lower_cap
 
 para_boss_lower:
-    INC objects_y_velocity, x
-
     LDA objects_y_velocity, x
-    CMP #50
+    CLC
+    ADC #(1 - OBJECT_FALLRATE)
+    STA objects_y_velocity, x
+
+para_boss_lower_cap:
+    LDA objects_y_velocity, x
+    CMP #40
     BMI +
-    LDA #50
+    LDA #40
     STA objects_y_velocity, x
 +
 
+    RTS
+
+para_boss_handle_below_player:
+    LDA objects_detection_flags, x
+    AND #object_hit_ground
+    BNE para_boss_handle_on_ground
+
+    JMP para_boss_rise
+
+para_boss_handle_on_ground:
+    LDA #-20
+    STA objects_y_velocity, x
     RTS
 
 
@@ -60,31 +132,20 @@ para_boss_handle_vertical_velocity:
 
     LDA Temp_Var14
     AND #%10000000
-    BEQ +
+    BNE +
     LDA Temp_Var15
-    BPL para_boss_rise_quickest     ; If below player, get above them no matter what
-    JMP ++
-+
-    LDA Temp_Var15
-    BPL para_boss_rise              ; Slower rise above the player
-
-++
-    LDA Temp_Var15
-    CLC
-    ADC para_boss_vertical_distance_ranges, y
-    BCS +
+    BPL para_boss_handle_below_player   ; The boss is below the player
 
 -
-    INY
-
-    LDA para_boss_vertical_distance_ranges, y
-    BEQ +
+; Loop through each value until we hit a value under the var
 
     LDA Temp_Var15
-    CLC
     ADC para_boss_vertical_distance_ranges, y
-    BCS +
-    BCC -
+    BPL +
+
+    INY
+    JMP -
+
 
 +
     LDA para_boss_vertical_distance_speeds, y
