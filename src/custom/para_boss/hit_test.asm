0f9a9af61dbd38c920eed3d47a16d79b86a0c070
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-11-17 21:28:47 -0500

    Add hittable states

--- src/custom/para_boss/hit_test.asm
+++ src/custom/para_boss/hit_test.asm
@@ -1,7 +1,15 @@
 
 ; 0 is hittable, 1 is not hittable
 para_state_hittable: 
-	.byte 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+	.byte 0, 0, 0, 0
+    .byte 0, 0, 0, 0
+    .byte 0, 0, 0, 0
+    .byte 0, 0, 0, 0
+
+    .byte 0, 0, 0, 0
+    .byte 0, 0, 0, 0
+    .byte 0, 0, 0, 0
+    .byte 1, 1, 0, 0
 
 para_hit_test:
     JSR Object_HandleBumpUnderneath  	; Handle getting bumped
