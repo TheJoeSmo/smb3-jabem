5c3195158429ed2c8134ce4b3591077de172ead4
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-11-17 21:27:58 -0500

    Fixed incorrect graphics

--- src/custom/para_boss/attributes/graphics_data.asm
+++ src/custom/para_boss/attributes/graphics_data.asm
@@ -3,4 +3,4 @@
 	.byte $87, $89, $8B, $A7, $A9, $AB
 	.byte $8D, $8F, $91, $AD, $AF, $B1
 	.byte $93, $95, $97, $B3, $B5, $B7
-	.byte $99, $9B, $9D, $B9, $9B, $9D
+	.byte $99, $9B, $9D, $B9, $BB, $BD
