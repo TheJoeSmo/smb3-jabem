19a21dc421a6f84069458fbc7b4459fb006358d6
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-11-19 16:00:49 -0500

    Add more hittable phases

--- src/custom/para_boss/hit_test.asm
+++ src/custom/para_boss/hit_test.asm
@@ -11,6 +11,11 @@ para_state_hittable:
     .byte 0, 0, 0, 0
     .byte 1, 1, 0, 0
 
+    .byte 0, 0, 0, 0
+    .byte 0, 0, 0, 0
+    .byte 0, 0, 0, 0
+    .byte 0, 0, 0, 0
+
 para_hit_test:
     JSR Object_HandleBumpUnderneath  	; Handle getting bumped
 
