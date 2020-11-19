75278d2bd2143da161fc83c28eff325f7c29b94e
TheJoeSmo <joesmo.joesmo12@gmail.com>
2020-11-19 15:59:37 -0500

    Add hit animation

--- src/custom/para_boss/action_get_hit.asm
+++ src/custom/para_boss/action_get_hit.asm
@@ -1,4 +1,10 @@
 
+
+para_horz_hit:
+    .byte -30, 30
+para_horz_boom_vel:
+	.byte -20, 20
+
 ; Boss falling out of sky animation
 para_boss_get_hit:	
 	LDA #$04
@@ -11,5 +17,19 @@ para_boss_get_hit:
 	JMP para_boss_dirty_update		; Do the next state	
 
 +
+
+	; Make a poof of smoke with some velocity
+	LDA #28
+	JSR summon_poof_from_object_spot
+	STY Temp_Var1
+	JSR Level_ObjCalcXDiffs
+	LDA para_horz_boom_vel, y
+
+	LDY Temp_Var1
+	STA SpecialObj_XVel, y
+
+	JSR Level_ObjCalcXDiffs
+    LDA para_horz_hit, y
+    STA objects_x_velocity, x
 
 	JMP para_boss_apply_velocity
