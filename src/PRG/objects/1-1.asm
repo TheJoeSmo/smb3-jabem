adede6cb37bdf7045e005e4953b4184a23629d9a
fortenbt <1106428+fortenbt@users.noreply.github.com>
2021-08-12 08:41:37 -0400

    World 1 map and level object fixes

--- src/PRG/objects/1-1.asm
+++ src/PRG/objects/1-1.asm
@@ -1,6 +1,7 @@
     .byte $01   ; Unknown purpose
 
-    .byte $0F, $0E, $17        ; Make para boss
+    .byte OBJ_GOOMBA, $0E, $19
+    .byte OBJ_VENUSFIRETRAP, $16, $17
     .byte OBJ_GOOMBA, $21, $19
     .byte OBJ_REDTROOPA, $24, $16
     .byte OBJ_GOOMBA, $34, $18
@@ -13,6 +14,5 @@
     .byte OBJ_GREENPIRANHA, $70, $18
     .byte OBJ_GREENPIRANHA_FIRE, $74, $17
     .byte OBJ_REDTROOPA, $83, $17
-    .byte OBJ_ENDLEVELCARD, $A8, $15
     .byte OBJ_ENDLEVELCARD, $A8, $15
     .byte $FF   ; Terminator
