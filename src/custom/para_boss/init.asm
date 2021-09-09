aba27253d697ec74d84ed1de7168827c649ac045
TheJoeSmo <joesmo.joesmo12@gmail.com>
2021-09-09 15:23:31 -0400

    Add new hit selection by x position The new types are 7 hits, 5 hits, 3 hits, and not killable. These are determined by x % 4, respectively.

--- src/custom/para_boss/init.asm
+++ src/custom/para_boss/init.asm
@@ -5,7 +5,30 @@ para_boss_flip:  		.byte SPR_HFLIP, $00
 para_boss_init:
     JSR Level_ObjCalcXDiffs
 
+    ; Not invincible by default
+    LDA #$00
+    STA objects_v14, x
+
+    LDA Objects_X, x
+    AND #%00000011
+    BNE +
+    LDA #$07
+    JMP ++
++
+    CMP #$01
+    BNE +
     LDA #$05
+    JMP ++
++
+    CMP #$02
+    BNE +
+    LDA #$03
+    JMP ++
++
+    LDA #$01    ; Cannot die
+    STA objects_v14, x
+    LDA #$05
+++
 	STA objects_health, x
 
 	LDA #$00
